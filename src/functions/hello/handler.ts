5ff636413b34e9e8b559b09e16939f1190c5e6e3
Luiz Spinelli <luiz.luchi@gmail.com>
2021-10-13 11:22:31 -0300

    response

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -4,9 +4,16 @@ import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event, context, callback,) => {
   return formatJSONResponse({
-    message: `Hello lambda2`
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: {
+      message: `Hello lambda2`
+    }
   });
 }
 
