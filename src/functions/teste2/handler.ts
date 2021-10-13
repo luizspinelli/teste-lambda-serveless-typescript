b866e100c11b0dba21f2abe92b0ccca65148b118
Luiz Spinelli <luiz.luchi@gmail.com>
2021-10-13 11:39:27 -0300

    response

---
+++ src/functions/teste2/handler.ts
@@ -0,0 +1,21 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
+import { formatJSONResponse } from '@libs/apiGateway';
+import { middyfy } from '@libs/lambda';
+
+import schema from './schema';
+
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event, context, callback,) => {
+
+  return formatJSONResponse({
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: {
+      message: `Teste`
+    }
+  });
+}
+
+export const main = middyfy(hello);
--- src/functions/teste2/index.ts
+++ src/functions/teste2/index.ts
@@ -6,7 +6,7 @@ export default {
     {
       http: {
         method: 'get',
-        path: '/',
+        path: '/teste',
         cors: true
       }
     }
